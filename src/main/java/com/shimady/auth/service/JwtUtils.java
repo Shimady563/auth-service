4cc4d0e5b7732f5a4d4eddcb32d61ab32d518e5a
Shimady563 <shimady563@gmail.com>
2025-01-06 20:18:20 +0300

    feat(jwt): create services and repos for jwt generation and validation

---
+++ src/main/java/com/shimady/auth/service/JwtUtils.java
@@ -0,0 +1,23 @@
+package com.shimady.auth.service;
+
+import com.shimady.auth.model.JwtAuthentication;
+import com.shimady.auth.model.Role;
+import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUtils {
+    public static JwtAuthentication generateAuthentication(Claims claims) {
+        JwtAuthentication auth = new JwtAuthentication();
+        auth.setEmail(claims.getSubject());
+        auth.setRole(Role.valueOf(claims.get("role", String.class)));
+        return auth;
+    }
+
+    public static String getTokenFromHeader(String header) {
+        if (header == null || !header.startsWith("Bearer ")) {
+            return null;
+        }
+        return header.substring(7);
+    }
+}
